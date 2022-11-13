c507daf0750bcdecca652ba98053341f52cff831
BrunaBelo <anabruna28@gmail.com>
2022-11-13 12:37:42 -0300

    refactor: move customer success filter tests from compact style to module

--- tests/filters/filter_valids_customer_success_tests.rb
+++ tests/filters/filter_valids_customer_success_tests.rb
@@ -1,36 +1,37 @@
 require 'minitest/autorun'
-require 'timeout'
 require './filters/filter_valids_customer_success.rb'
 
-class Filters::FilterValidsCustomerSuccessTests < Minitest::Test
-  def test_allowed_ids_filter
-    filter = described_class.new(
-      customer_success: [
-        { id: 10, score: 10 },
-        { id: 200_000, score: 20 },
-        { id: 3, score: 30_000 }
-      ],
-      away_customer_success: []
-    )
-    assert_equal [{ id: 10, score: 10 }], filter.filter
-  end
+module Filters
+  class FilterValidsCustomerSuccessTests < Minitest::Test
+    def test_allowed_ids_filter
+      filter = described_class.new(
+        customer_success: [
+          { id: 10, score: 10 },
+          { id: 200_000, score: 20 },
+          { id: 3, score: 30_000 }
+        ],
+        away_customer_success: []
+      )
+      assert_equal [{ id: 10, score: 10 }], filter.filter
+    end
 
-  def test_without_away_cs
-    filter = described_class.new(
-      customer_success: [
-        { id: 1, score: 1 },
-        { id: 10, score: 10 },
-        { id: 200_000, score: 20 },
-        { id: 3, score: 30_000 }
-      ],
-      away_customer_success: [10]
-    )
-    assert_equal [{ id: 1, score: 1 }], filter.filter
-  end
+    def test_without_away_cs
+      filter = described_class.new(
+        customer_success: [
+          { id: 1, score: 1 },
+          { id: 10, score: 10 },
+          { id: 200_000, score: 20 },
+          { id: 3, score: 30_000 }
+        ],
+        away_customer_success: [10]
+      )
+      assert_equal [{ id: 1, score: 1 }], filter.filter
+    end
 
-  private
+    private
 
-  def described_class
-    Filters::FilterValidsCustomerSuccess
+    def described_class
+      Filters::FilterValidsCustomerSuccess
+    end
   end
 end
